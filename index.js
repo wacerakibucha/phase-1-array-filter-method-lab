63143bb0b9203a96f217b8305712f3198fb356e0
wacerakibucha <beatricekibucha@gmail.com>
2025-09-10 13:41:06 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+// Match the 'name' property of driver objects
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string);
+}
+
+const driversArray = ["Bobby", "Sam", "Samantha", "Rob"];
+
+console.log(findMatching(driversArray, "sAm")); // => ["Sam"]
+console.log(fuzzyMatch(driversArray, "Sa")); // => ["Sam", "Samantha"]
+
+const driverObjects = [
+  { name: "Bobby", hometown: "New York" },
+  { name: "Sam", hometown: "Boston" },
+];
+console.log(matchName(driverObjects, "Sam")); 
+
